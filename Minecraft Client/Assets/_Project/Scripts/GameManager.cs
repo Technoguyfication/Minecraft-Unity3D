0e6d7ce41f9dc8e6635a0e47496a211aa6ff1a5f
Hayden Andreyka <haydenandreyka@gmail.com>
2018-11-09 14:41:43 -0700

    getting prepared to work on game manager

---
+++ Minecraft Client/Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	[Header("Prefabs")]
+	public GameObject PlayerPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void Awake()
+	{
+		// make sure the game manager stays when we change the level
+		DontDestroyOnLoad(this);
+	}
+
+	/// <summary>
+	/// Signals the game manager to connect to a server
+	/// </summary>
+	public void ConnectToServer()
+	{
+
+	}
+}
