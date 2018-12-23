e3d387917713bf982c8a28eba00cc29eabd9c30c
Hayden Andreyka <haydenandreyka@gmail.com>
2018-12-23 11:11:42 -0700

    implement minecraft-style hash algorithm

---
+++ Minecraft Client/Assets/_Project/Scripts/Protocol/EncryptionUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Used for protocol encryption
+/// </summary>
+public class EncryptionUtility
+{
+	/// <summary>
+	/// Generates a minecraft-style hash of a string
+	/// </summary>
+	/// <param name="input"></param>
+	/// <returns></returns>
+	public string Hash(string input)
+	{
+		using (var sha = SHA1.Create())
+		{
+			byte[] digest = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+			return new BigInteger(digest.ReverseIfLittleEndian()).ToString("x");	// don't know if we always need to reverse this or not
+		}
+	}
+}
