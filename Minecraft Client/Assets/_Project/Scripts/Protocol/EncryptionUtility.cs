85d2cf5a5fca53dbc3ee297483635fb0e3d06791
Hayden Andreyka <haydenandreyka@gmail.com>
2018-12-23 23:44:52 -0700

    finish aes encryption

--- Minecraft Client/Assets/_Project/Scripts/Protocol/EncryptionUtility.cs
+++ Minecraft Client/Assets/_Project/Scripts/Protocol/EncryptionUtility.cs
@@ -6,32 +6,92 @@ using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 using CSharp_easy_RSA_PEM;
+using System.IO;
 
 /// <summary>
 /// Used for protocol encryption
 /// </summary>
 public class EncryptionUtility
 {
-	private const int KEY_SIZE = 1024;
-	private readonly RSACryptoServiceProvider _rsaProvider;
-
-	/// <summary>
-	/// Creates a new encryption class
-	/// </summary>
-	/// <param name="publicKey">An X509</param>
-	public EncryptionUtility(string publicKey)
-	{
-		_rsaProvider = Crypto.DecodeX509PublicKey(publicKey);
-	}
+	private const int SHARED_SECRET_SIZE = 16;
+	private Aes _aesProvider;
+	private ICryptoTransform _aesEncryptTransform;
+	private ICryptoTransform _aesDecryptTransform;
 
 	/// <summary>
 	/// Encrypts data using the server's public key
 	/// </summary>
 	/// <param name="data"></param>
 	/// <returns></returns>
-	public byte[] EncryptRSA(byte[] data)
+	public byte[] EncryptRSA(byte[] data, string publicKey)
 	{
-		return _rsaProvider.Encrypt(data, true);
+		using (var rsa = Crypto.DecodeX509PublicKey(publicKey))
+		{
+			return rsa.Encrypt(data, true);
+		}
+	}
+
+	/// <summary>
+	/// Encrypts data using the shared AES key
+	/// </summary>
+	/// <param name="data"></param>
+	/// <returns></returns>
+	public byte[] EncryptAES(byte[] data)
+	{
+		using (var ms = new MemoryStream())
+		{
+			using (var cs = new CryptoStream(ms, _aesEncryptTransform, CryptoStreamMode.Write))
+			{
+				cs.Write(data, 0, data.Length);
+				cs.Close();
+				return ms.ToArray();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Decrypts data using AES
+	/// </summary>
+	/// <param name="data"></param>
+	/// <returns></returns>
+	public byte[] DecryptAES(byte[] data)
+	{
+		using (var ms = new MemoryStream())
+		{
+			using (var cs = new CryptoStream(ms, _aesDecryptTransform, CryptoStreamMode.Write))
+			{
+				cs.Write(data, 0, data.Length);
+				cs.Close();
+				return ms.ToArray();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Sets the AES key and IV so we can start using AES encryption
+	/// </summary>
+	public void SetAESKey(byte[] sharedKey)
+	{
+		_aesProvider = Aes.Create();
+		_aesProvider.Key = sharedKey;
+		_aesProvider.IV = sharedKey;
+
+		_aesEncryptTransform = _aesProvider.CreateEncryptor();
+		_aesDecryptTransform = _aesProvider.CreateDecryptor();
+	}
+
+	/// <summary>
+	/// Gets a shared secret for use in AES symmetric encryption
+	/// </summary>
+	/// <returns></returns>
+	public static byte[] GetSharedSecret()
+	{
+		using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+		{
+			byte[] secret = new byte[SHARED_SECRET_SIZE];
+			rng.GetBytes(secret);
+			return secret;
+		}
 	}
 
 	/// <summary>
