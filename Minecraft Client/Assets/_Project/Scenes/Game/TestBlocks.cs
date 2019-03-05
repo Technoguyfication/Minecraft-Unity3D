2b9e721c4fd3bbc0b3bb9d9098f241a515b54b09
Hayden Andreyka <haydenandreyka@gmail.com>
2019-03-05 13:39:44 -0700

    Add some ambient lighting to game scene

---
+++ Minecraft Client/Assets/_Project/Scenes/Game/TestBlocks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBlocks : MonoBehaviour
+{
+	public bool DestroyOnStart = true;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (DestroyOnStart)
+			Destroy(this);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
