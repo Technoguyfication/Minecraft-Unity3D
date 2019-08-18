678c9c7ec23ecfba4db7c3d821e5dc6f60540607
Hayden Andreyka <haydenandreyka@gmail.com>
2019-08-18 01:11:13 -0700

    finish merge

--- Minecraft Client/Assets/_Project/Scripts/UI/PlayerEntry.cs
+++ Minecraft Client/Assets/_Project/Scripts/UI/PlayerEntry.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerEntry : MonoBehaviour
 {
+	public TextMeshProUGUI DisplayName;
 
+	public void UpdateEntry(Player player)
+	{
+		// set display name
+		if (player.DisplayName != null)
+			DisplayName.text = ChatComponent.GetFormattedString(player.DisplayName, null);
+		else
+			DisplayName.text = player.Name;
+
+		DisplayName.text += $" {player.Ping.ToString()}";
+	}
 }
--- Minecraft Client/Assets/_Project/Scripts/UI/PlayerList.cs
+++ Minecraft Client/Assets/_Project/Scripts/UI/PlayerList.cs
@@ -8,7 +8,7 @@ public class PlayerList : MonoBehaviour
 	public GameObject PlayerListPanel;
 	public KeyCode PlayerListKey = KeyCode.Tab;
 
-	public PlayerEntry PlayerEntryPrefab;
+	public GameObject PlayerEntryPrefab;
 
 	// Start is called before the first frame update
 	void Start()
@@ -35,6 +35,17 @@ public class PlayerList : MonoBehaviour
 	/// <param name="players"></param>
 	public void UpdatePlayerList(Player[] players)
 	{
-		Debug.Log($"adding {players.Length} players");
+		// clear all player entries
+		foreach (Transform child in PlayerListPanel.transform)
+		{
+			Destroy(child.gameObject);
+		}
+
+		// create player entries
+		foreach (var player in players)
+		{
+			var playerEntry = Instantiate(PlayerEntryPrefab, PlayerListPanel.transform).GetComponent<PlayerEntry>();
+			playerEntry.UpdateEntry(player);
+		}
 	}
 }
